a08d29e15b5125e9da53a9e8b4775c9505e5e3e0
pawelrubin <pawelrubin19@gmail.com>
2019-10-10 21:06:17 +0200

    Zadanie 2.

---
+++ lab01/zad02.jl
@@ -0,0 +1,14 @@
+TYPES = [Float16, Float32, Float64]
+
+function kahan_eps(type)
+    type(type(3)*(type(4)/type(3) - one(type)) - one(type))
+end
+
+foreach(type -> 
+    println(
+        kahan_eps(type),
+        " vs ",
+        eps(type)
+    ),
+    TYPES
+)
