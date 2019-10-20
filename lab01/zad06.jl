32fcc1f16771490602dc7685b841306574723e7e
pawelrubin <pawelrubin19@gmail.com>
2019-10-20 14:39:38 +0200

    Zadanie 6 - poprawki * Uproszczenie funkcji (Float64 jest domyślną arytmetyką) * Drukowanie w stylu tabel LaTeX

--- lab01/zad06.jl
+++ lab01/zad06.jl
@@ -1,20 +1,20 @@
 function f(x) # odejmowanie bliskich wartosci
-    sqrt(x^2 + one(Float64)) - one(Float64)    
+    sqrt(x^2 + 1) - 1    
 end
 
 function g(x) 
-    x^2 / (sqrt(x^2 + one(Float64)) + one(Float64))
+    x^2 / (sqrt(x^2 + 1) + 1)
 end
 
 function f_vs_g(n)
-    for i = one(Float64):n
+    for i = 1:n
         x = Float64(8.0) ^ (-i)
         _f = f(x)
         _g = g(x)
         if _f != _g
-            println("i = $i, x = $x; f(x): $_f vs $_g :(x)g")
+            println("$i & $_f & $_g \\\\ \\hline")
         end
     end
 end
 
-f_vs_g(100)
+f_vs_g(200)
