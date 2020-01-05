a162ab3718ec87747ee147239e56aae635725bcc
Paweł Rubin <pawelrubin19@gmail.com>
2020-01-05 20:49:00 +0100

    Testy modułu blocksys.

--- lab05/blocksys.jl
+++ lab05/blocksys.jl
@@ -1,49 +1,7 @@
 module blocksys
-export readMatrixFromFile, readVectorFromFile, gauss!, gaussPivoted!, solveWithGauss, solveWithPivotedGauss
+export gauss!, gaussPivoted!, solveWithGauss, solveWithPivotedGauss
 
 using SparseArrays
-
-
-"""
-Reads matrix from file.
-"""
-function readMatrixFromFile(file::IOStream)
-    sn, sl = split(readline(file))
-
-    n = parse(Int64, sn)
-    l = parse(Int64, sl)
-
-    rows    = Int64[]
-    columns = Int64[]
-    values  = Float64[]
-
-    for line in eachline(file)
-        row, column, value = split(line)
-        push!(rows,     parse(Int64, row)       )
-        push!(columns,  parse(Int64, column)    )
-        push!(values,   parse(Float64, value)   )
-    end
-
-    M = sparse(rows, columns, values)
-    return (M, n, l)
-end
-
-
-"""
-Reads vector from file.
-"""
-function readVectorFromFile(file::IOStream)
-    words = split(readline(file))
-    n = parse(Int, words[1])
-
-    b = Array{Float64}(undef, n)
-
-    for (i, line) in enumerate(eachline(file))
-        b[i] = parse(Float64, line)
-    end
-
-    return b    
-end
 
 
 """
---
+++ lab05/tests/test_blocksys.jl
@@ -0,0 +1,32 @@
+include("../blocksys.jl")
+include("../io_handler.jl")
+using .blocksys
+using .IOHandler
+
+using Test
+
+function testSolvers(solvers)
+    testDataIndexes = [16, 10000, 50000]
+    testData = []
+
+    # read test data
+    for index in testDataIndexes
+        matrixFile = open("./data/_$(index)/A.txt")
+        vectorFile = open("./data/_$(index)/b.txt")
+        (M, n, l) = readMatrixFromFile(matrixFile)
+        b = readVectorFromFile(vectorFile)
+        push!(testData, (M, b, n, l))
+    end
+
+    foreach(solver -> 
+        @testset "$solver" begin
+            foreach(test -> 
+                @test isapprox(solver(test...), ones(Float64, test[3]))
+            , testData)
+        end
+    , solvers)
+end
+
+solvers = [solveWithGauss, solveWithPivotedGauss]
+
+testSolvers(solvers)
